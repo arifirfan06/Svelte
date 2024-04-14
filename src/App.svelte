de95e235ddc9d691604549581ad2a9074b8559f9
Irfan <irfan@Irfans-MacBook-Air.local>
2024-04-14 18:32:39 +0700

    finish v1

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+
+
+import Counter from "./lib/Counter.svelte";
+
+// use this props with ... / spread
+const props = {
+  initCount: 0,
+  maxCount: 5
+}
+</script>
+
+<Counter {...props} other='not used yet'/>
+
+<style>
+
+</style>
